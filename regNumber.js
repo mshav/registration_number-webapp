00dbce0a147ddd2454ece901ffbdd024b600f0c1
mshav <mbulelobakana@gmail.com>
2017-09-15 16:14:40 +0200

    I tryed to change something but it did n't worked

--- regNumber.js
+++ regNumber.js
@@ -1,31 +1,67 @@
-  module.exports = function(models) {
+module.exports = function(models) {
+  plateList = [];
+  plateList2 = [];
   const regNo = function(req, res) {
-    res.render('regNo')
+    models.Plate.find({}, function(err, result) {
+      if (err) {
+        return done(err)
+      }
+      console.log(result)
+      res.render('regNo', {
+        msg: result
+      })
+    })
   }
 
   const regNo1 = function(req, res, done) {
     var name = req.body.name;
     var regMsg = '';
 
-     models.Plate.create({
-       reg_number: name
-     }, function (err) {
-       if (err) {
-         return done(err)
-       }
-       models.Plate.find({}, function (err, result) {
-         if (err) {
 
-         }
-         
+    models.Plate.create({
+      reg_number: name
+    }, function(err, result) {
+      if (err) {
+        return done(err)
+      }
 
-         console.log(result);
-         res.render('regNo', {
-           msg: result
-         });
 
-       })
-     })
+      result.save(function(err, result) {
+        models.Plate.find({
+
+          reg_number: name
+
+        }, function(err, result) {
+
+          if (err) {
+
+            return done(err)
+          }
+
+          if (name == name) {
+
+           plateList.push(name)
+           for (var i = 0; i < plateList.length; i++) {
+           for (var j = 0; j < plateList2.length; j++) {
+          if (plateList[i] == plateList2 && i != j){
+
+            return done(plateList2)
+          }
+           }
+           }
+
+           return done(plateList2)
+
+
+          }
+
+          console.log(plateList2);
+          res.render('regNo', {
+            msg: plateList.result
+          });
+        })
+      })
+    });
   }
 
   return {
